0775c23c517c53fad38c317afe8f639d419e177e
Lance <ljpruitt123@gmail.com>
2024-04-29 17:36:05 -0700

    fix: deploy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Specify the correct entry module here
     rollupOptions: {
-      input: 'frontend/src/main.jsx',
+      input: "src/main.jsx",
     },
   },
 });
-
