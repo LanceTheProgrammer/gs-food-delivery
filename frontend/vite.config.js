a20c12fc394cf0cb1593a8ca5eafe6ea8ccde180
Lance <ljpruitt123@gmail.com>
2024-06-19 17:36:53 -0700

    Setup for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Specify the correct entry module here
-    rollupOptions: {
-      input: "src/main.jsx",
-    },
+    outDir: 'dist',
   },
 });
